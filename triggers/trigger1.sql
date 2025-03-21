eb6fc36dcfea4210699991ba5ab0626fdf2e477d
Vidhipatel1125 <patel.vidhide@northeastern.edu>
2025-03-21 18:12:35 -0400

    Add code for Total Amount Update

--- triggers/trigger1.sql
+++ triggers/trigger1.sql
@@ -1,1 +1,11 @@
 
+#Update Order Total Amount When an Order Item is Added or Updated
+CREATE OR REPLACE TRIGGER update_order_total
+AFTER INSERT OR UPDATE ON Order_Items
+FOR EACH ROW
+BEGIN
+  UPDATE Customer_Orders
+  SET total_amount = (SELECT SUM(quantity * unit_price) FROM Order_Items WHERE order_id = :NEW.order_id)
+  WHERE order_id = :NEW.order_id;
+END;
+/
