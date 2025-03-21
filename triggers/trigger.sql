b094e50eeaf9eba4bf83f24195583db229c8759d
Vidhipatel1125 <patel.vidhide@northeastern.edu>
2025-03-21 17:47:06 -0400

    Add SQL code to trigger1.sql

---
+++ triggers/trigger.sql
@@ -0,0 +1,12 @@
+
+#trigger for updating the inventory 
+CREATE OR REPLACE TRIGGER trg_update_inventory_after_order
+AFTER INSERT ON Order_Items
+FOR EACH ROW
+BEGIN
+  UPDATE Inventory
+  SET stock_level = stock_level - :NEW.product_quantity
+  WHERE inventory_id = (SELECT inventory_id FROM Products WHERE product_id = :NEW.Products_product_id);
+END;
+
+
