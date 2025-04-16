781220047ad5db0de4321a5c55ab1ef7aaea3bc3
dharanakneu <kashyap.dh@northeastern.edu>
2025-04-16 10:10:21 -0400

    Add trg for inventory threshold check

---
+++ TRIGGERS/triggers.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE TRIGGER trg_inventory_threshold_check
+AFTER UPDATE ON Inventory
+FOR EACH ROW
+WHEN (NEW.stock_level < NEW.reorder_threshold)
+BEGIN
+    INSERT INTO Inventory_Threshold_Log (inventory_id, product_id, stock_level, threshold)
+    SELECT i.inventory_id, p.product_id, :NEW.stock_level, :NEW.reorder_threshold
+    FROM Products p
+    JOIN Inventory i ON p.inventory_id = i.inventory_id
+    WHERE i.inventory_id = :NEW.inventory_id;
+END;
+/
+
+CREATE OR REPLACE TRIGGER trg_prevent_discount_update
+BEFORE UPDATE ON Discounts
+FOR EACH ROW
+BEGIN
+    RAISE_APPLICATION_ERROR(-20001, 'Discounts are immutable and cannot be updated.');
+END;
+/
+
+CREATE OR REPLACE TRIGGER trg_prevent_discount_delete
+BEFORE DELETE ON Discounts
+FOR EACH ROW
+BEGIN
+    RAISE_APPLICATION_ERROR(-20002, 'Discounts are immutable and cannot be deleted.');
+END;
+/
